d6a6ff67732d84cc090d246e9169973a9fc004e1
Sofia989 <noichl.sofiya@gmail.com>
2025-07-21 12:17:45 +0200

    folder structure for Page Object Model

---
+++ src/test/java/theinternet/core/TestBase.java
@@ -0,0 +1,19 @@
+package theinternet.core;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TestBase {
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void init(){
+        driver=new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+}
